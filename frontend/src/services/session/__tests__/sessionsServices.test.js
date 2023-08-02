615ae483c8d1f22a609b14d98f2b9393d646bb98
Mairuwka <maiorov23@yandex.ru>
2023-08-02 21:25:32 +0300

    fix after review

--- frontend/src/services/session/__tests__/sessionsServices.test.js
+++ frontend/src/services/session/__tests__/sessionsServices.test.js
@@ -13,47 +13,36 @@ describe("SessionService", () => {
     sessionsService = null;
   });
 
-  describe("Check instance", () => {
+  describe("Constructor", () => {
     it("Checking for instance", () => {
       expect(sessionsService).toBeInstanceOf(SessionsService);
     });
   });
 
-  describe("Test getSessions method", () => {
-    it("should call setSessions and return sessions from LocalStorage", () => {
-      sessionsService.setSessions = jest.fn();
-
+  describe("getSessions method", () => {
+    it("Should call setSessions if there is no saved date with sessions", () => {
       const date = dayjs().format("YYYY-MM-DD");
+      const setSessionsSpy = jest.spyOn(sessionsService, "setSessions");
 
-      jest.spyOn(LocalStorage, "get").mockReturnValueOnce(null);
+      sessionsService.getSessions(date);
+
+      expect(setSessionsSpy).toHaveBeenCalled();
+      expect(setSessionsSpy).toHaveBeenCalledWith(dayjs(date));
+    });
+
+    it("Should create sessions and return them", () => {
+      const date = dayjs().format("YYYY-MM-DD");
+      const getSessionsSpy = jest.spyOn(sessionsService, "getSessions");
 
       const result = sessionsService.getSessions(date);
 
-      expect(sessionsService.setSessions).toHaveBeenCalledWith(dayjs(date));
-
-      expect(result).toBeNull();
+      expect(getSessionsSpy).toHaveBeenCalled();
+      expect(getSessionsSpy).toHaveBeenCalledWith(date);
+      expect(result).not.toHaveLength(0);
     });
 
-    it("should return sessions from this.sessions if available", () => {
-      const date = dayjs().format("YYYY-MM-DD");
-      const sessions = [
-        { id: 1, name: "Session 1" },
-        { id: 2, name: "Session 2" },
-      ];
-
-      jest.spyOn(sessionsService, "getSessions").mockReturnValueOnce(sessions);
-
-      sessionsService.sessions[date] = sessions;
-
-      const result = sessionsService.getSessions(date);
-
-      expect(result).toEqual(sessions);
-    });
-
-    it("should return null for non-existing dates", () => {
-      const date = dayjs().format("YYYY-MM-DD");
-
-      jest.spyOn(sessionsService, "getSessions").mockReturnValueOnce(null);
+    it("Should return null if the date is not between the week before and the week after", () => {
+      const date = dayjs().add(8, "day").format("YYYY-MM-DD");
 
       const result = sessionsService.getSessions(date);
 
